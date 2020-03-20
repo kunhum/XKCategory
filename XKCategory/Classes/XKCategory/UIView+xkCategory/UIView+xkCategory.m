f542899342cee6affc216f00943501dc46792bb3
xieweikun <kunhum@163.com>
2020-03-20 16:43:09 +0800

    Release 1.2.6

--- XKCategory/Classes/XKCategory/UIView+xkCategory/UIView+xkCategory.m
+++ XKCategory/Classes/XKCategory/UIView+xkCategory/UIView+xkCategory.m
@@ -7,7 +7,6 @@
 //
 
 #import "UIView+xkCategory.h"
-#import "UIColor+ZXLazy.h"
 
 @implementation UIView (xkCategory)
 
@@ -70,17 +69,17 @@
         
         if (self.traitCollection.userInterfaceStyle == UIUserInterfaceStyleDark) {
             
-            return [UIColor colorWithHexString:darkColor];
+            return [UIView colorWithHexString:darkColor];
         }
         else {
             
-            return [UIColor colorWithHexString:lightColor];
+            return [UIView colorWithHexString:lightColor];
         }
         
     } else {
         // Fallback on earlier versions
         
-        return [UIColor colorWithHexString:lightColor];
+        return [UIView colorWithHexString:lightColor];
     }
 }
 
@@ -99,6 +98,48 @@
     }
     
     self.layer.mask = shareLayer;
+}
+
++ (UIColor *)colorWithHexString:(NSString *)color {
+    
+    NSString *cString = [[color stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] uppercaseString];
+    
+    // String should be 6 or 8 characters
+    if ([cString length] < 6) {
+        return [UIColor clearColor];
+    }
+    
+    // strip 0X if it appears
+    if ([cString hasPrefix:@"0X"])
+        cString = [cString substringFromIndex:2];
+    if ([cString hasPrefix:@"#"])
+        cString = [cString substringFromIndex:1];
+    if ([cString length] != 6)
+        return [UIColor clearColor];
+    
+    // Separate into r, g, b substrings
+    NSRange range;
+    range.location = 0;
+    range.length = 2;
+    
+    //r
+    NSString *rString = [cString substringWithRange:range];
+    
+    //g
+    range.location = 2;
+    NSString *gString = [cString substringWithRange:range];
+    
+    //b
+    range.location = 4;
+    NSString *bString = [cString substringWithRange:range];
+    
+    // Scan values
+    unsigned int r, g, b;
+    [[NSScanner scannerWithString:rString] scanHexInt:&r];
+    [[NSScanner scannerWithString:gString] scanHexInt:&g];
+    [[NSScanner scannerWithString:bString] scanHexInt:&b];
+    
+    return [UIColor colorWithRed:((float) r / 255.0f) green:((float) g / 255.0f) blue:((float) b / 255.0f) alpha:1.0f];
 }
 
 @end
