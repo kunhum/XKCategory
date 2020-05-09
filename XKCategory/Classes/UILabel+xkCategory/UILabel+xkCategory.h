71992ad0202ed832544172a45e2bae15f43a44eb
xieweikun <kunhum@163.com>
2020-05-09 10:57:49 +0800

    Release 1.3.2

--- XKCategory/Classes/UILabel+xkCategory/UILabel+xkCategory.h
+++ XKCategory/Classes/UILabel+xkCategory/UILabel+xkCategory.h
@@ -12,5 +12,7 @@
 
 ///是否不需要自适应高度，默认为NO
 @property (nonatomic, assign) BOOL doNotAdjustFont;
+///将后面的显示...且在最后添加suffixText
+- (void)xk_setLineBreakByTruncatingLastLineWithWidth:(CGFloat)width suffixText:(NSString *)suffixText suffixTextColor:(UIColor *)suffixTextColor lineSpacing:(CGFloat)lineSpacing;
 
 @end
--- XKCategory/Classes/UILabel+xkCategory/UILabel+xkCategory.m
+++ XKCategory/Classes/UILabel+xkCategory/UILabel+xkCategory.m
@@ -60,4 +60,89 @@
     return number.boolValue;
 }
 
+- (void)xk_setLineBreakByTruncatingLastLineWithWidth:(CGFloat)width suffixText:(NSString *)suffixText suffixTextColor:(UIColor *)suffixTextColor lineSpacing:(CGFloat)lineSpacing {
+    
+    if ( self.numberOfLines <= 0 ) {
+        return;
+    }
+    NSArray *separatedLines = [self getSeparatedLinesArray:width];
+    
+    NSMutableString *limitedText = [NSMutableString string];
+    
+    NSString *omitText = @"...";
+    
+    if (separatedLines.count > self.numberOfLines) {
+        
+        for (int i = 0 ; i < self.numberOfLines; i++) {
+            
+            //最后一行，到达限制的一行
+            if ( i == self.numberOfLines - 1) {
+                
+                NSArray *subSeparatedLines  = separatedLines;
+                NSString *lastLineText      = subSeparatedLines[i];
+                NSInteger lastLineTextCount = lastLineText.length;
+                
+                if ([[lastLineText substringFromIndex:(lastLineTextCount - 1)] isEqualToString:@"\n"]) {
+                    lastLineText = [lastLineText substringToIndex:(lastLineText.length - 1)];
+                    lastLineTextCount = lastLineTextCount - 1;
+                }
+                
+                NSInteger length = suffixText.length + 1;
+                [limitedText appendString:[lastLineText substringToIndex:lastLineText.length - length]];
+            }
+            //未到达限制
+            else {
+                [limitedText appendString:separatedLines[i]];
+            }
+        }
+        
+    }
+    else {
+        [limitedText appendString:self.text];
+    }
+    
+    NSMutableParagraphStyle *style = [[NSMutableParagraphStyle alloc] init];
+    style.lineSpacing = lineSpacing;
+    NSMutableAttributedString *attText = [[NSMutableAttributedString alloc] initWithString: (( separatedLines.count > self.numberOfLines ) ? [limitedText stringByAppendingString:[NSString stringWithFormat:@"%@%@", omitText, suffixText]] : limitedText) attributes:@{ NSForegroundColorAttributeName : self.textColor, NSFontAttributeName : self.font, NSParagraphStyleAttributeName : style }];
+    if ( separatedLines.count > self.numberOfLines ) {
+        [attText addAttributes:@{
+                                NSFontAttributeName : [UIFont systemFontOfSize:15*XK_RATIO],
+                                NSForegroundColorAttributeName : suffixTextColor
+                                } range:NSMakeRange(attText.length - 4, 4)];
+    }
+    
+    self.attributedText = attText;
+}
+
+- (NSArray *)getSeparatedLinesArray:(CGFloat)width {
+    
+    NSString *text = [self text];
+    UIFont   *font = [self font];
+    
+    CTFontRef myFont = CTFontCreateWithName((__bridge CFStringRef)([font fontName]), [font pointSize], NULL);
+    NSMutableAttributedString *attStr = [[NSMutableAttributedString alloc] initWithString:text];
+    [attStr addAttribute:(NSString *)kCTFontAttributeName value:(__bridge id)myFont range:NSMakeRange(0, attStr.length)];
+    
+    CTFramesetterRef frameSetter = CTFramesetterCreateWithAttributedString((__bridge CFAttributedStringRef)attStr);
+    
+    CGMutablePathRef path = CGPathCreateMutable();
+    CGPathAddRect(path, NULL, CGRectMake(0, 0, width, 100000));
+    
+    CTFrameRef frame = CTFramesetterCreateFrame(frameSetter, CFRangeMake(0, 0), path, NULL);
+    
+    NSArray *lines = (__bridge NSArray *)CTFrameGetLines(frame);
+    NSMutableArray *linesArray = [[NSMutableArray alloc]init];
+    
+    for (id line in lines) {
+        CTLineRef lineRef = (__bridge CTLineRef )line;
+        CFRange lineRange = CTLineGetStringRange(lineRef);
+        NSRange range = NSMakeRange(lineRange.location, lineRange.length);
+        
+        NSString *lineString = [text substringWithRange:range];
+        [linesArray addObject:lineString];
+    }
+       
+    return (NSArray *)linesArray;
+}
+
 @end
